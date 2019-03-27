fb58979aa529a916c2e93256c447bee2e6114c50
Filipe <filipecheverrya@gmail.com>
2019-03-27 02:27:07 -0300

    customize tag a that already exist in body and add img

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 var footer_logo = '<a href="https://thrivedatascience.com" target="_blank" title="Thrive Data Science"><img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" /></a>',
-    header_logo = '<a href="https://jenkins.thrivedatascience.com/" title="Thrive Data Science"><img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" /></a>',
+    header_logo = '<img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" />',
     contact_info = '<a href="mailto:comercial@thrivedatascience.com" title="Thrive Data Science" class="contact-lnk">comercial@thrivedatascience.com</a>'
 
 contact_info += '<a href="tel:+5553997039577" title="Thrive Data Science" class="contact-lnk">+55 53 9 9703 9577</a>'
@@ -9,7 +9,9 @@ function footerLogo() {
     document.querySelector('body>footer .page_generated').innerHTML = footer_logo;
 }
 function headerLogo() {
-    document.getElementById('jenkins-home-link').innerHTML = header_logo;
+    var elem = document.getElementById('jenkins-home-link');
+    elem.setAttribute('href', 'https://jenkins.thrivedatascience.com/');
+    elem.innerHTML = header_logo;
 }
 function footerContact() {
     document.getElementById('footer').innerHTML = contact_info;
