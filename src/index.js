6fe72d4e36533f5d1047d9c1fe2aff93926825ae
Filipe <filipecheverrya@gmail.com>
2019-03-27 01:54:59 -0300

    insert header and footer logo as function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+var logo = '<a href="https://thrivedatascience.com" target="_blank" alt="Thrive Data Science"><img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" /></a>'
+
+function footerLogo() {
+    document.querySelector('body>footer .page_generated').innerHTML = logo;
+}
+function headerLogo() {
+    document.getElementById('jenkins-home-link').innerHTML = logo;
+}
+
 setTimeout(() => {
-    var footerLogo = '<a href="https://thrivedatascience.com" target="_blank" alt="Thrive Data Science"><img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" /></a>'
-    document.querySelector('body>footer .col-md-18').innerHTML = footerLogo;
+    footerLogo();
+    headerLogo();
 }, 750);
