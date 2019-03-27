bb1e7b62fae4f403e0a4ee23a1cf5e228cec9452
Filipe <filipecheverrya@gmail.com>
2019-03-27 02:12:38 -0300

    add contact link

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-var footerLogo = '<a href="https://thrivedatascience.com" target="_blank" alt="Thrive Data Science"><img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" /></a>'
-var headerLogo = '<a href="https://jenkins.thrivedatascience.com/" alt="Thrive Data Science"><img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" /></a>'
+var footerLogo = '<a href="https://thrivedatascience.com" target="_blank" title="Thrive Data Science"><img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" /></a>',
+    headerLogo = '<a href="https://jenkins.thrivedatascience.com/" title="Thrive Data Science"><img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" /></a>',
+    contactInfo = '<a href="mailto:comercial@thrivedatascience.com" title="Thrive Data Science" class="contact-lnk">comercial@thrivedatascience.com</a>'
+
+contactInfo += '<a href="tel:+5553997039577" title="Thrive Data Science" class="contact-lnk">+55 53 9 9703 9577</a>'
 
 function footerLogo() {
     document.querySelector('body>footer .page_generated').innerHTML = footerLogo;
@@ -7,8 +10,12 @@ function footerLogo() {
 function headerLogo() {
     document.getElementById('jenkins-home-link').innerHTML = headerLogo;
 }
+function footerContact() {
+    document.getElementById('footer').innerHTML = contactInfo;
+}
 
 setTimeout(() => {
     footerLogo();
     headerLogo();
+    footerContact();
 }, 750);
