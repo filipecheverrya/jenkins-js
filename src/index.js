bfa00b10acbfc8c2c227c9f365e2e3123fe5dcb2
Filipe <filipecheverrya@gmail.com>
2019-03-27 00:51:31 -0300

    set delay

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-var footerLogo = '<a href="https://thrivedatascience.com" target="_blank" alt="Thrive Data Science"><img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" /></a>'
-document.querySelector('body>footer .col-md-18').innerHTML = footerLogo;
+setTimeout(() => {
+    var footerLogo = '<a href="https://thrivedatascience.com" target="_blank" alt="Thrive Data Science"><img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" /></a>'
+    document.querySelector('body>footer .col-md-18').innerHTML = footerLogo;
+}, 750);
