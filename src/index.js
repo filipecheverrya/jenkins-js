f0f07ca86bfbfdd85a04bb7db52903a9e9789db6
Filipe <filipecheverrya@gmail.com>
2019-03-27 01:57:03 -0300

    set home link on header logo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-var logo = '<a href="https://thrivedatascience.com" target="_blank" alt="Thrive Data Science"><img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" /></a>'
+var footerLogo = '<a href="https://thrivedatascience.com" target="_blank" alt="Thrive Data Science"><img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" /></a>'
+var headerLogo = '<a href="https://jenkins.thrivedatascience.com/" alt="Thrive Data Science"><img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" /></a>'
 
 function footerLogo() {
-    document.querySelector('body>footer .page_generated').innerHTML = logo;
+    document.querySelector('body>footer .page_generated').innerHTML = footerLogo;
 }
 function headerLogo() {
-    document.getElementById('jenkins-home-link').innerHTML = logo;
+    document.getElementById('jenkins-home-link').innerHTML = headerLogo;
 }
 
 setTimeout(() => {
