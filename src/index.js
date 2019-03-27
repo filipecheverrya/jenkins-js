b3fe5cde522bc82b39544617cda978ec098cb576
Filipe <filipecheverrya@gmail.com>
2019-03-27 00:45:04 -0300

    nossa, sou mt burro

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 var footerLogo = '<a href="https://thrivedatascience.com" target="_blank" alt="Thrive Data Science"><img src="https://thrivedatascience.com/images/logo.png" alt="Thrive Data Science" width="120" height="40" /></a>'
-document.querySelector('footer>body .col-md-18').innerHTML = footerLogo;
+document.querySelector('body>footer .col-md-18').innerHTML = footerLogo;
