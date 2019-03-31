fea466f572e96dac340a410791debe1e8b25e216
Filipe <filipecheverrya@gmail.com>
2019-03-31 20:44:32 -0300

    add svg on logo with css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import mainCss from './css/index.css'
-import logoJenkins from './img/jenkins-logo.svg'
 import logoThrive from './img/thrive-logo.svg'
 
 const footerContent =
@@ -9,22 +8,18 @@ const footerContent =
         <a href="https://thrivedatascience.com/" class="footer-logo-thrive" target="_blank" title="Thrive Data Science">${logoThrive}</a>
     </div>
     <div class="col-md-16">
-        <a href="https://jenkins.thrivedatascience.com/" class="jenkins-lnk" target="_blank" title="Jenkins">JENKINS</a>
+        <a href="https://jenkins.thrivedatascience.com/" class="jenkins-lnk" target="_blank" title="Jenkins">Jenkins</a>
         <a href="mailto:support@thrivedatascience.com" class="jenkins-lnk" title="support@thrivedatascience.com">support@thrivedatascience.com</a>
     </div>
 </div>
 `,
-    headerLogo = 
-`
-<a href="https://jenkins.thrivedatascience.com/" class="header-jenkins-logo" title="Jenkins">${logoJenkins}</a>
-`
 
 function footerLogo() {
     document.querySelector('body>footer .container-fluid .row').insertAdjacentHTML('beforebegin', footerContent)
 }
 function logoHeader() {
-    var elem = document.querySelector('#header .logo');
-    elem.innerHTML = headerLogo;
+    var elem = document.querySelector('#header .logo > a');
+    elem.setAttribute('href', 'https://jenkins.thrivedatascience.com/').classList.add('header-jenkins-logo')
 }
 
 setTimeout(() => {
