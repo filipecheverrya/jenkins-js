9a39da105b66d45b0c36b297e0d484b51229d276
Filipe <filipecheverrya@gmail.com>
2019-03-31 20:07:54 -0300

    change support position

--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ import logoThrive from './img/thrive-logo.svg'
 const footerContent =
 `
 <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-16">
         <a href="https://thrivedatascience.com/" class="footer-logo-thrive" target="_blank" title="Thrive Data Science">${logoThrive}</a>
     </div>
-    <div class="col-md-8">
-        <a href="mailto:support@thrivedatascience.com" class="contact-lnk" title="support@thrivedatascience.com">support@thrivedatascience.com</a>
-    </div>
-    <div class="col-md-8">
+    <div class="col-md-16">
         <a href="https://jenkins.thrivedatascience.com/" class="jenkins-lnk" target="_blank" title="Jenkins">JENKINS</a>
+        <a href="mailto:support@thrivedatascience.com" class="jenkins-lnk" title="support@thrivedatascience.com">support@thrivedatascience.com</a>
     </div>
 </div>
 `,
